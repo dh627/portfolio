f03398fb05f0d7bfbd85ec6c117ab7b9916f843b
Telmo Sampaio <telmoasampaio@gmail.com>
2021-06-30 19:46:55 +0100

    Fixing mobile menu

---
+++ main.js
@@ -0,0 +1,11 @@
+const burgerIcon = document.getElementsByClassName('burger-link')[0];
+const mobileNav = document.getElementById('mobileNavLinks');
+
+burgerIcon.addEventListener('click', () => {
+
+  if(mobileNav.classList.contains('showMobileNav')) {
+    mobileNav.classList.remove('showMobileNav');
+  } else {
+    mobileNav.classList.add('showMobileNav');
+  }
+})
